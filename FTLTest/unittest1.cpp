6839b2324039e9897a2684b36fc08ef669a50bb0
Seung Ho Jang <shjang1992@gmail.com>
2015-11-06 22:20:26 +0900

    Main project & test project added

---
+++ FTLTest/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "EnumEnumerator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FTLTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
