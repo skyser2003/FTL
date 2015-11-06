bb7a3711b3b3bc931dbbb9c6e1faa2172b7cf280
Seung Ho Jang <shjang1992@gmail.com>
2015-11-06 23:46:36 +0900

    Property added

---
+++ FTL/Property.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template <class Type, class Setter, class Getter>
+class Property
+{
+
+};
---
+++ FTLTest/unittest2.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "Property.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace FTLTest
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
