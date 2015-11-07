26e3136979b0dd41ab9c16227e6842b09c8ed2c8
Seung Ho Jang <shjang1992@gmail.com>
2015-11-08 00:05:18 +0900

    Property class roughly done

--- FTL/Property.h
+++ FTL/Property.h
@@ -1,7 +1,167 @@
 #pragma once
 
-template <class Type, class Setter, class Getter>
-class Property
+template <class Type>
+class DefaultSetter
 {
+public:
+	Type operator()(const Type& rhs)
+	{
+		return rhs;
+	}
+};
 
+template <class Type>
+class DefaultGetter
+{
+public:
+	Type operator()(const Type& rhs)
+	{
+		return rhs;
+	}
+};
+
+// Declare
+template <class OwnerClass, class Type, bool setPrivate, bool getPrivate>
+class Property;
+
+// Both public
+template <class OwnerClass, class Type>
+class Property<OwnerClass, Type, false, false>
+{
+public:
+	Property() :Property(DefaultSetter<Type>(), DefaultGetter<Type>())
+	{
+
+	}
+
+	Property(std::function<Type(Type)> setter, std::function<Type(Type)> getter)
+	{
+		this->setter = setter;
+		this->getter = getter;
+	}
+
+	Type operator=(const Type& rhs)
+	{
+		return value = setter(rhs);
+	}
+
+	operator Type() const
+	{
+		return getter(value);
+	}
+
+private:
+	Type value;
+
+	std::function<Type(Type)> setter;
+	std::function<Type(Type)> getter;
+};
+
+// Setter private, getter public
+template <class OwnerClass, class Type>
+class Property<OwnerClass, Type, true, false>
+{
+public:
+	friend OwnerClass;
+
+	Property() :Property(DefaultSetter<Type>(), DefaultGetter<Type>())
+	{
+
+	}
+
+	Property(std::function<Type(Type)> setter, std::function<Type(Type)> getter)
+	{
+		this->setter = setter;
+		this->getter = getter;
+	}
+
+private:
+	Type operator=(const Type& rhs)
+	{
+		return value = setter(rhs);
+	}
+
+public:
+	operator Type() const
+	{
+		return getter(value);
+	}
+
+private:
+	Type value;
+
+	std::function<Type(Type)> setter;
+	std::function<Type(Type)> getter;
+};
+
+// Setter public, Getter private
+template <class OwnerClass, class Type>
+class Property<OwnerClass, Type, false, true>
+{
+public:
+	friend OwnerClass;
+
+	Property() :Property(DefaultSetter<Type>(), DefaultGetter<Type>())
+	{
+
+	}
+
+	Property(std::function<Type(Type)> setter, std::function<Type(Type)> getter)
+	{
+		this->setter = setter;
+		this->getter = getter;
+	}
+
+	Type operator=(const Type& rhs)
+	{
+		return value = setter(rhs);
+	}
+
+private:
+	operator Type() const
+	{
+		return getter(value);
+	}
+
+private:
+	Type value;
+
+	std::function<Type(Type)> setter;
+	std::function<Type(Type)> getter;
+};
+
+// Both private
+template <class OwnerClass, class Type>
+class Property<OwnerClass, Type, true, true>
+{
+public:
+	friend OwnerClass;
+
+	Property() :Property(DefaultSetter<Type>(), DefaultGetter<Type>())
+	{
+
+	}
+
+	Property(std::function<Type(Type)> setter, std::function<Type(Type)> getter)
+	{
+		this->setter = setter;
+		this->getter = getter;
+	}
+
+private:
+	Type operator=(const Type& rhs)
+	{
+		return value = setter(rhs);
+	}
+
+	operator Type() const
+	{
+		return getter(value);
+	}
+
+private:
+	Type value;
+
+	std::function<Type(Type)> setter;
+	std::function<Type(Type)> getter;
 };
--- FTLTest/unittest2.cpp
+++ FTLTest/unittest2.cpp
@@ -8,11 +8,17 @@ using namespace std;
 
 namespace FTLTest
 {
-	TEST_CLASS(UnitTest2)
+	TEST_CLASS(PropertyTest)
 	{
 	public:
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(PropertyTest1)
 		{
+			char buffer[25];
+			Property<PropertyTest, int, true, true> prop([](int val) {return val*val; }, DefaultGetter<int>());
+			Logger::WriteMessage(itoa((prop = 3), buffer, 10));
+			Logger::WriteMessage("\n");
+			Logger::WriteMessage(itoa(prop, buffer, 10));
+			Logger::WriteMessage("\n");
 		}
 	};
 }
