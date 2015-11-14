a8f1564be884858514999a72b42424cddc1a17ba
Seung Ho Jang <shjang1992@gmail.com>
2015-11-15 04:08:01 +0900

    Property unit test modified

--- FTLTest/unittest2.cpp
+++ FTLTest/unittest2.cpp
@@ -9,17 +9,45 @@ using namespace FTL;
 
 namespace FTLTest
 {
+	class TestClass
+	{
+	public:
+		Property<TestClass, int, false, false> defaultProp;
+		Property<TestClass, int, true, true> prop1{ [](int val) {return val*val; }, DefaultGetter<int>() };
+		Property<TestClass, int, true, false> prop2{ [](int val) {return val*val; }, DefaultGetter<int>() };
+		Property<TestClass, int, false, true> prop3{ [](int val) {return val*val; }, DefaultGetter<int>() };
+		Property<TestClass, int, false, false> prop4{ [](int val) {return val*val; }, DefaultGetter<int>() };
+
+		void Test()
+		{
+			prop1 = 1;
+			prop2 = 2;
+			prop3 = 3;
+			prop4 = 4;
+		}
+	};
+
 	TEST_CLASS(PropertyTest)
 	{
 	public:
 		TEST_METHOD(PropertyTest1)
 		{
-			char buffer[25];
-			Property<PropertyTest, int, true, true> prop([](int val) {return val*val; }, DefaultGetter<int>());
-			Logger::WriteMessage(itoa((prop = 3), buffer, 10));
-			Logger::WriteMessage("\n");
-			Logger::WriteMessage(itoa(prop, buffer, 10));
-			Logger::WriteMessage("\n");
+			FTLTest::TestClass cls;
+			cls.Test();
+
+			// Getter
+			Assert::AreEqual(static_cast<int>(cls.defaultProp), 0);
+			// Assert::AreEqual(static_cast<int>(cls.prop1), 1 * 1); // Compile error
+			Assert::AreEqual(static_cast<int>(cls.prop2), 2 * 2);
+			// Assert::AreEqual(static_cast<int>(cls.prop3), 3 * 3); // Compile error
+			Assert::AreEqual(static_cast<int>(cls.prop4), 4 * 4);
+
+			// Setter
+			Assert::AreEqual(static_cast<int>(cls.defaultProp = 1), 1);
+			// Assert::AreEqual(static_cast<int>(cls.prop1 = 1), 1 * 1); // Compile error
+			// Assert::AreEqual(static_cast<int>(cls.prop2 = 2), 2 * 2); // Compile error
+			Assert::AreEqual(static_cast<int>(cls.prop3 = 3), 3 * 3);
+			Assert::AreEqual(static_cast<int>(cls.prop4 = 4), 4 * 4);
 		}
 	};
 }
