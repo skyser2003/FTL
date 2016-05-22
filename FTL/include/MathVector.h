3f68dadfb54d5b96eb268ec474e6879bd7b74e7f
Seung Ho Jang <shjang1992@gmail.com>
2016-05-22 16:44:48 +0900

    MathVector class added Should be adjusted

---
+++ FTL/include/MathVector.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include "Property.h"
+
+namespace FTL
+{
+	template <class ValueType, int Dimension>
+	class Vector
+	{
+	public:
+		typedef ValueType ValueType;
+
+		virtual ~Vector() {}
+
+		Property<Vector, ValueType, false, false> Value[Dimension];
+
+		void Set(ValueType valueArray[Dimension])
+		{
+			for (int i = 0; i < Dimension; ++i)
+			{
+				Value[i] = valueArray[i];
+			}
+		}
+
+		void Set(int index, ValueType value)
+		{
+			if (IsOutOfRange(index) == true)
+			{
+				return;
+			}
+
+			Value[index] = value;
+		}
+
+		ValueType Get(int index) const
+		{
+			if (IsOutOfRange(index) == true)
+			{
+				return ValueType();
+			}
+
+			return Value[index];
+		}
+
+		bool IsInRange(int index) const
+		{
+			return 0 <= index && index < Dimension;
+		}
+
+		bool IsOutOfRange(int index) const
+		{
+			return IsInRange(index) == false;
+		}
+	};
+
+	template <class ValueType>
+	class Vector2 : public Vector<ValueType, 2>
+	{
+	public:
+		virtual ~Vector2() override {}
+
+		Property<Vector2, ValueType, false, false> X
+		{
+			[this](ValueType value) { return Value[0]; },
+			[this](ValueType value) { return Value[0] = value; }
+		};
+
+		Property<Vector2, ValueType, false, false> Y
+		{
+			[this](ValueType value) { return Value[1]; },
+			[this](ValueType value) { return Value[1] = value; }
+		};
+
+		void Set(ValueType x, ValueType y)
+		{
+			X = x;
+			Y = y;
+		}
+	};
+
+	template <class ValueType>
+	class Vector3 : public Vector<ValueType, 3>
+	{
+	public:
+		virtual ~Vector3() override {}
+
+		Property<Vector3, ValueType, false, false> X
+		{
+			[this](ValueType value) { return Value[0]; },
+			[this](ValueType value) { return Value[0] = value; }
+		};
+
+		Property<Vector3, ValueType, false, false> Y
+		{
+			[this](ValueType value) { return Value[1]; },
+			[this](ValueType value) { return Value[1] = value; }
+		};
+
+		Property<Vector3, ValueType, false, false> Z
+		{
+			[this](ValueType value) { return Value[2]; },
+			[this](ValueType value) { return Value[2] = value; }
+		};
+
+		void Set(ValueType x, ValueType y, ValueType z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+	};
+
+	using Vector2i = Vector2<int>;
+	using Vector3i = Vector3<int>;
+
+	using Vector2f = Vector2<float>;
+	using Vector3f = Vector3<float>;
+}
---
+++ FTLTest/src/mathvector_test.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "MathVector.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace FTL;
+
+namespace FTLTest
+{
+	TEST_CLASS(MathVectorTest)
+	{
+	public:
+		TEST_METHOD(MathVectorTest1)
+		{
+			Vector2i vec2i;
+			Vector3i vec3i;
+			Vector2f vec2f;
+			Vector3f vec3f;
+
+			vec2i.Value[0] = 1;
+			vec2i.Value[1] = 2;
+
+			vec3i.Value[0] = 4;
+			vec3i.Value[1] = 5;
+			vec3i.Value[2] = 6;
+
+			vec2f.Value[0] = 1.5f;
+			vec2f.Value[1] = 2.5f;
+
+			vec3f.Value[0] = 4.5f;
+			vec3f.Value[1] = 5.5f;
+			vec3f.Value[2] = 6.5f;
+
+			// Int
+			Assert::AreEqual(1, static_cast<int>(vec2i.Value[0]));
+			Assert::AreEqual(2, static_cast<int>(vec2i.Value[1]));
+
+			Assert::AreEqual(1, static_cast<int>(vec2i.X));
+			Assert::AreEqual(2, static_cast<int>(vec2i.Y));
+
+			Assert::AreEqual(4, static_cast<int>(vec3i.Value[0]));
+			Assert::AreEqual(5, static_cast<int>(vec3i.Value[1]));
+			Assert::AreEqual(6, static_cast<int>(vec3i.Value[2]));
+
+			Assert::AreEqual(4, static_cast<int>(vec3i.X));
+			Assert::AreEqual(5, static_cast<int>(vec3i.Y));
+			Assert::AreEqual(6, static_cast<int>(vec3i.Z));
+			Assert::AreEqual(1, static_cast<int>(vec2i.Value[0]));
+			Assert::AreEqual(2, static_cast<int>(vec2i.Value[1]));
+
+			// Float
+			Assert::AreEqual(1.5f, static_cast<float>(vec2f.X));
+			Assert::AreEqual(2.5f, static_cast<float>(vec2f.Y));
+
+			Assert::AreEqual(4.5f, static_cast<float>(vec3f.Value[0]));
+			Assert::AreEqual(5.5f, static_cast<float>(vec3f.Value[1]));
+			Assert::AreEqual(6.5f, static_cast<float>(vec3f.Value[2]));
+
+			Assert::AreEqual(4.5f, static_cast<float>(vec3f.X));
+			Assert::AreEqual(5.5f, static_cast<float>(vec3f.Y));
+			Assert::AreEqual(6.5f, static_cast<float>(vec3f.Z));
+		}
+	};
+}
