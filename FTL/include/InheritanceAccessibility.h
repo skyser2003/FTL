d572799a6e4a45a422fdfe8180e8ae52843b3932
Seung Ho Jang <shjang1992@gmail.com>
2017-04-02 12:06:13 +0900

    InheritanceAccessibility implemented

--- FTL/include/InheritanceAccessibility.h
+++ FTL/include/InheritanceAccessibility.h
@@ -1,14 +1,22 @@
 #pragma once
 
+#include <type_traits>
+
 namespace FTL
 {
 	// Declare
 	template <class... Args>
 	class InheritanceAccessiblity;
 
-	template <class... Args, class ab>
-	class InheritanceAccessiblity<ab, Args...>
-	{
-	public:
-	};
+	// Public
+	template <class CLS, class... Args>
+	class InheritanceAccessiblity<std::true_type, CLS, Args...> : public CLS, public InheritanceAccessiblity<Args...> {};
+
+	// Private
+	template <class CLS, class... Args>
+	class InheritanceAccessiblity<std::false_type, CLS, Args...> : private CLS, public InheritanceAccessiblity<Args...> {};
+
+	// End
+	template <>
+	class InheritanceAccessiblity<> {};
 }
--- FTLTest/src/accessibility_test.cpp
+++ FTLTest/src/accessibility_test.cpp
@@ -9,11 +9,61 @@ using namespace FTL;
 
 namespace FTLTest
 {
+	class Pub
+	{
+	public:
+		constexpr bool IsPublic() const
+		{
+			return true;
+		}
+	};
+
+	class Pri
+	{
+	public:
+		constexpr bool IsPrivate() const
+		{
+			return true;
+		}
+	};
+
+	template <class T, class = void>
+	class CheckPublicCls : public false_type {};
+
+	template <class T>
+	class CheckPublicCls<T, void_t<decltype(declval<T>().IsPublic())>> : public true_type {};
+
+	template <class T, class = void>
+	class CheckPrivateCls : public false_type {};
+
+	template <class T>
+	class CheckPrivateCls<T, void_t<decltype(declval<T>().IsPrivate())>> : public true_type {};
+
+	template <class T>
+	constexpr bool CheckPublic(const T&)
+	{
+		return typename CheckPublicCls<T>::value;
+	}
+
+	template <class T>
+	constexpr bool CheckPrivate(const T&)
+	{
+		return typename CheckPrivateCls<T>::value;
+	}
+
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		UnitTest1()
+		TEST_METHOD(AccessibilityTest1)
 		{
+			InheritanceAccessiblity<false_type, Pri, true_type, Pub> p1;
+			InheritanceAccessiblity<true_type, Pub, false_type, Pri> p2;
+
+			Assert::AreEqual(true, CheckPublic(p1));
+			Assert::AreEqual(false, CheckPrivate(p1));
+
+			Assert::AreEqual(true, CheckPublic(p2));
+			Assert::AreEqual(false, CheckPrivate(p2));
 		}
 	};
 }
